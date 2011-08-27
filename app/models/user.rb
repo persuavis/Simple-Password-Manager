0bf9ce919635db91399b4b17093888f9163f5aea
Wim den Braven <wimdenbraven@persuavis.com>
2011-08-27 16:43:02 -0700

    added password encryption to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,32 @@ class User < ActiveRecord::Base
   
   validates_presence_of :username
   validates_length_of :username, :minimum => 3
+  validates_presence_of :encrypted_password
+  validates_presence_of :salt
+  
+  attr_accessor :password
+  
+  def password=(pwd)
+    @password = pwd
+    if pwd.present?
+      self.encrypted_password = self.class.encrypt_number(@password, generate_salt)
+    else
+      self.encrypted_password = nil
+    end
+  end
+  
+  def password
+    @password
+  end
+  
+private
+
+  def self.encrypt_number(number, salt) 
+    Digest::SHA2.hexdigest(number + "spm1" + salt)
+  end
+  
+  def generate_salt
+    self.salt = (self.object_id.to_s + rand.to_s) unless self.salt
+  end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,9 @@ module UserSpecHelper
   def valid_user_attributes
     {
       :username => "goodname",
-      :email => "goodname@email.com"
+      :email => "goodname@email.com",
+      :encrypted_password => "dc8c4afee22ef525bf3269d27ff25888052da76b2c8dcaab7e7e6af3efc13f59",
+      :salt => '21696922800.8241008911277153'
     }
   end
 end
@@ -24,6 +26,8 @@ describe User, "with good attributes" do
   it "should have no errors" do
     @user.should have(0).error_on(:username)
     @user.should have(0).error_on(:email)
+    @user.should have(0).error_on(:encrypted_password)
+    @user.should have(0).error_on(:salt)
   end
 end
 
@@ -41,6 +45,8 @@ describe User, "with empty attributes" do
   it "should have errors" do
     @user.should have(2).error_on(:username)
     @user.should have(0).error_on(:email)
+    @user.should have(1).error_on(:encrypted_password)
+    @user.should have(1).error_on(:salt)
   end
 end
 
@@ -78,5 +84,21 @@ describe User, "with a blank username" do
   end
 end
 
+describe User, "with a blank password" do
+  include UserSpecHelper
+  
+  before(:each) do
+    @user = User.new
+    @user.attributes = valid_user_attributes.except(:encrypted_password)
+  end
+
+  it do
+    @user.should_not be_valid
+  end
+  
+  it "should have one error on encrypted_password" do
+    @user.should have(1).error_on(:encrypted_password)
+  end
+end
 
 
