61e0b254d4ab013d4740768d5fbc761221f345a9
Wim den Braven <wimdenbraven@persuavis.com>
2011-08-27 14:06:12 -0700

    initialized SimpleProjectManager/spm1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplePasswordManager::Application.config.secret_token = '04fab79a7c6840d9840af27ae7f56988a0b4c167d6af74e718e87685ad06e7b63bf041b12ac168e8122396e4c5bd3846bed5fed4e18937fd4db964870d280369'
